d7b109a093fbd080638b64af3cf83bdcd0c7849e
BrunoWillSoares <bruno.p.soares@gmail.com>
2021-09-07 23:28:00 -0300

    Ajuste Imagens

--- src/main/java/com/tienda/business/CelularBusiness.java
+++ src/main/java/com/tienda/business/CelularBusiness.java
@@ -11,10 +11,10 @@ public class CelularBusiness {
 	public List<Celular> obterCelulares() {
 		List<Celular> celulares = new ArrayList<Celular>();
 		
-		Celular iPhone = new Celular(1,"Apple", "iPhone 12 Pro Max 256GB", new BigDecimal(200), new BigDecimal(9000), "imagens/iphone.jpg");
-		Celular s21 = new Celular(1,"Samsung", "Galaxy S21 Ultra", new BigDecimal(200), new BigDecimal(6100), "imagens/s21-ultra.jpg");
-		Celular note = new Celular(1,"Samsung", "Galaxy Note 20", new BigDecimal(200), new BigDecimal(3100), "imagens/note20.png");
-		Celular motorola = new Celular(1,"Motorola", "Edge 20 Pro", new BigDecimal(200), new BigDecimal(4999), "imagens/motorola_edge.png");
+		Celular iPhone = new Celular(1,"Apple", "iPhone 12 Pro Max 256GB", new BigDecimal(200), new BigDecimal(9000), "./imagens/iphone.jpg");
+		Celular s21 = new Celular(1,"Samsung", "Galaxy S21 Ultra", new BigDecimal(200), new BigDecimal(6100), "./imagens/s21-ultra.jpg");
+		Celular note = new Celular(1,"Samsung", "Galaxy Note 20", new BigDecimal(200), new BigDecimal(3100), "./imagens/note20.png");
+		Celular motorola = new Celular(1,"Motorola", "Edge 20 Pro", new BigDecimal(200), new BigDecimal(4999), "./imagens/motorola_edge.png");
 		
 		celulares.add(iPhone);
 		celulares.add(s21);
